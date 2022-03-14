0b226cd9678119e43aa319da7726b797e0a2be33
Pu Zheng <zhengpuas47@gmail.com>
2022-03-14 10:35:56 -0400

    fixed errors in dataorganization

---
+++ run_scripts/20220312-MOp_mendel_cellpose.bat
@@ -0,0 +1,12 @@
+call activate merlin_test
+merlin -a 20220312-MOp_cellpose.json ^
+-o 20220312-M1_22bits_adaptors.csv ^
+-c M1_codebook.csv ^
+-m storm6_microscope.json ^
+-p 20220312_positions.txt ^
+-e I:\MERFISH_Data ^
+-s I:\MERFISH_Analysis ^
+-n 44 ^
+--no_report True ^
+20220312-P_brain_M1_adaptors
+pause
