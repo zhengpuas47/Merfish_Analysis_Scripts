0dc29b06081f50e8a73b5a390483b9221b46549e
Pu Zheng <zhengpuas47@gmail.com>
2022-03-19 18:00:24 -0400

    mendel examples for 0314

---
+++ run_scripts/20220314-MOp_mendel_cellpose.bat
@@ -0,0 +1,12 @@
+call activate merlin_test
+merlin -a 20220312-MOp_cellpose.json ^
+-o 20220314-M1_22bits_adaptors.csv ^
+-c M1_codebook.csv ^
+-m storm6_microscope.json ^
+-p 20220314_positions.txt ^
+-e I:\MERFISH_Data ^
+-s I:\MERFISH_Analysis ^
+-n 55 ^
+--no_report True ^
+20220314-P_brain_M1_adaptors
+pause
