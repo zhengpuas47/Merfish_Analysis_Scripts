b900458e58ae4d3077643958d02a09832064e737
Pu Zheng <zhengpuas47@gmail.com>
2022-03-29 17:20:11 -0400

    update 0328

---
+++ run_scripts/20220328-MOp_mendel_cellpose.bat
@@ -0,0 +1,12 @@
+call activate merlin_test
+merlin -a 20220312-MOp_cellpose.json ^
+-o 20220328-M1_22bits_adaptors.csv ^
+-c M1_codebook.csv ^
+-m storm6_microscope.json ^
+-p 20220328_positions.txt ^
+-e I:\MERFISH_Data ^
+-s I:\MERFISH_Analysis ^
+-n 55 ^
+--no_report True ^
+20220328-P_brain_M1_nonclear_adaptors
+pause
