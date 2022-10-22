cbeb9beed6905b84685b24f87cd665281c5420e0
Pu Zheng <zhengpuas47@gmail.com>
2022-10-22 02:33:42 -0400

    20221012 hmtg organization

---
+++ run_scripts/20221011-hMTG_hM1_mendel_cellpose.bat
@@ -0,0 +1,12 @@
+call activate merlin_test
+merlin -a 20221003-MOp_cellpose.json ^
+-o 20221003-hM1_22bits_adaptors.csv ^
+-c human_M1_codebook.csv ^
+-m storm6_microscope.json ^
+-p 20221011_positions.txt ^
+-e J:\MERFISH_Data ^
+-s I:\MERFISH_Analysis ^
+-n 66 ^
+--no_report True ^
+20221011-hMTG_hM1
+pause
